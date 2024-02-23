c5b506bdb8fcd536da356200720a840f5d315923
Daniel Koitka <daniel_koitka@outlook.com>
2024-02-23 23:27:26 +1000

    added permision denying

--- globFileSystemTest.cpp
+++ globFileSystemTest.cpp
@@ -6,7 +6,8 @@ void TestFileSystem(std::string_view glob, std::vector<std::filesystem::path>& o
     std::string rootPath = std::string(Glob::GetRootPathFromGlob(glob));
     std::cout << glob << " >> " << rootPath << std::endl;
 
-    for(auto currentDirectory : std::filesystem::recursive_directory_iterator(rootPath))
+    std::filesystem::directory_options options = std::filesystem::directory_options::follow_directory_symlink | std::filesystem::directory_options::skip_permission_denied;
+    for(auto currentDirectory : std::filesystem::recursive_directory_iterator(rootPath, options))
     {
         std::string pathFromDirectory = currentDirectory.path();
 
